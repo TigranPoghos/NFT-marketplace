5c446ddaaf431a7ba9c81b6813f4978ecc63f568
TigranPoghos <tigran2002poghosyan@gmail.com>
2024-05-08 18:59:52 +0300

    js

--- script/script.js
+++ script/script.js
@@ -32,4 +32,83 @@ document.addEventListener("DOMContentLoaded", function(){
             }
         });
     });
+
+
+    const ButtonArtwork = document.querySelector('.ButtonArtwork');
+    const ButtonBook = document.querySelector('.ButtonBook');
+    const bids__cards = document.querySelectorAll('.bids__card');
+    const ButtonAll = document.querySelector('.ButtonAll');
+
+    ButtonArtwork?.addEventListener('click', function() {
+        bids__cards?.forEach(function(bids__card) {
+            if (bids__card?.classList.contains('artwork')) {
+                bids__card?.classList.remove('active');
+                ButtonArtwork?.classList.add('active')
+                ButtonBook?.classList.remove('active')
+                ButtonAll?.classList.remove('active')
+            } else {
+                bids__card?.classList.add('active');
+            }
+        });
+    });
+
+    ButtonBook?.addEventListener('click', function() {
+        bids__cards?.forEach(function(bids__card) {
+            if (bids__card?.classList.contains('book')) {
+                bids__card?.classList.remove('active');
+                ButtonBook?.classList.add('active')
+                ButtonAll?.classList.remove('active')
+                ButtonArtwork?.classList.remove('active')
+            } else {
+                bids__card?.classList.add('active');
+            }
+        });
+    });
+
+    ButtonAll?.addEventListener('click', function() {
+        bids__cards?.forEach(function(bids__card) {
+            bids__card?.classList.remove('active');
+            ButtonAll?.classList.add('active')
+            ButtonArtwork?.classList.remove('active')
+            ButtonBook?.classList.remove('active')
+        });
+    });
+
+
+
+
+    //follow/unfollow
+    const ActivityButtons = document.querySelectorAll('.ActivityButton');
+    ActivityButtons?.forEach(ActivityButton => {
+        ActivityButton?.addEventListener('click', function() {
+        const hasFollowClass = ActivityButton?.classList.contains('Follow');
+            if (hasFollowClass) {
+                ActivityButton?.classList.remove('Follow');
+                ActivityButton?.classList.add('Unfollow');
+                ActivityButton.innerHTML = 'Unfollow';
+            } else {
+                ActivityButton?.classList.remove('Unfollow');
+                ActivityButton?.classList.add('Follow');
+                ActivityButton.innerHTML = 'Follow';
+            }
+        });
+    });
+
+
+
+
+
+
+    //setting
+    const SettingButtons = document.querySelectorAll('.SettingButton')
+    SettingButtons?.forEach(SettingButton => {
+        SettingButton?.addEventListener('click', function() {
+            SettingButtons?.forEach(btn => btn?.classList.remove('active'));
+            SettingButton?.classList.add('active')
+        })
+    })
+
+
+
+
 })
