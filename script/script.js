0b1400d8d756f976bad7d301bba40451e35c0963
TigranPoghos <tigran2002poghosyan@gmail.com>
2024-05-08 18:00:06 +0300

    js

--- script/script.js
+++ script/script.js
@@ -1,11 +1,35 @@
 document.addEventListener("DOMContentLoaded", function(){
-    const LightMode = document.querySelector('.LightMode')
-    const DarkMode = document.querySelector('.DarkMode')
+
+    //светлый и тёмный режимы
+    const LightMode = document.querySelector('.LightMode');
+    const DarkMode = document.querySelector('.DarkMode');
     const body = document.body;
-    LightMode.addEventListener('click', function(){
-        body.classList.add('active')
-    })
-    DarkMode.addEventListener('click', function(){
-        body.classList.remove('active')
-    })
+    const isLightMode = localStorage.getItem('theme') === 'dark';
+    if (!isLightMode) {
+        body.classList.add('active');
+    }
+    LightMode.addEventListener('click', function() {
+        localStorage.setItem('theme', 'light');
+        body.classList.add('active');
+    });
+    DarkMode.addEventListener('click', function() {
+        localStorage.setItem('theme', 'dark');
+        body.classList.remove('active');
+    });
+
+
+
+
+
+
+    //удаление строк таблицы
+    const DeleteButtons = document.querySelectorAll('.ActiveDelete');
+    DeleteButtons?.forEach(function(button) {
+        button.addEventListener('click', function() {
+            const Activetable = button.closest('.ActiveTable');
+            if (Activetable) {
+                Activetable?.classList.add('active');
+            }
+        });
+    });
 })
